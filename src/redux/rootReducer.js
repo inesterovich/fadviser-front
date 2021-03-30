8e724aa97df76e0b495e6df548a4d164e731d823
Ilya Nesterovich <manager@inesterovich.ru>
2021-03-30 13:05:59 +0300

    feat: add redux-persist

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,5 +1,7 @@
 import { combineReducers } from '@reduxjs/toolkit';
 import { counterSlice } from '../features/counter/counterSlice';
+
+
 export const rootReducer = combineReducers({
   counter: counterSlice.reducer
 })
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,8 +1,21 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import { rootReducer } from './rootReducer';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
-export const store = configureStore({
-  reducer: rootReducer,
+const persistConfig = {
+  key: 'root',
+  storage
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
   devTools: process.env.NODE_ENV !== 'production'
 })
+
+const persistor = persistStore(store);
+
+export { store, persistor };
 
