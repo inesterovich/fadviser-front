1607fb32960f3edf3e1f5d852e62f344e91af357
Ilya Nesterovich <manager@inesterovich.ru>
2021-04-18 17:45:51 +0300

    feat: add ENVIRONMENT_VARIABLES, update scripts

--- src/redux/api.requests.ts
+++ src/redux/api.requests.ts
@@ -1,11 +1,12 @@
 import { fetchRequest } from '../utils';
 
 
+const baseURL = process.env.REACT_APP_REST_API;
 // Нужная функция fetchRequest. Так совершенно невозможно
 export const registrationRequest = async (formData: any) => {
   
   try {
-  const response = await fetchRequest('http://127.0.0.1:5000/users/register', 'POST', formData);
+  const response = await fetchRequest(`${baseURL}/users/register`, 'POST', formData);
   return response.status;
   } catch (error) {
     return 500;
@@ -16,7 +17,7 @@ export const registrationRequest = async (formData: any) => {
 
 export const AuthorizationRequest = async (formData: any) => {
   try {
-    const response = await fetchRequest('http://127.0.0.1:5000/users/login', 'POST', formData);
+    const response = await fetchRequest(`${baseURL}/users/login`, 'POST', formData);
     return response;
   } catch (error) {
     return {
@@ -30,7 +31,7 @@ export const AuthorizationRequest = async (formData: any) => {
 export const UserDataRequest = async (userId:string, token:string) => {
   try {
     // У клиента id так-то нету. При логине лучше высылать ещё и id - тут это не дубль
-    const response = await fetchRequest(`http://127.0.0.1:5000/users/${userId}`, 'GET', null, {
+    const response = await fetchRequest(`${baseURL}/users/${userId}`, 'GET', null, {
       Authorization: `Bearer: ${token}`
     });
     return response;
